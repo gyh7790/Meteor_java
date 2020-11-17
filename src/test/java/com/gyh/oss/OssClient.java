22f2e03c3d9b8747d026df94e276e0a58793cb78
gyh7790 <gyh7790@qq.com>
2020-11-17 08:12:28 +0800

    阿里云 文件上传 示例

---
+++ src/test/java/com/gyh/oss/OssClient.java
@@ -0,0 +1,50 @@
+package com.gyh.oss;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.internal.OSSHeaders;
+import com.aliyun.oss.model.CannedAccessControlList;
+import com.aliyun.oss.model.ObjectMetadata;
+import com.aliyun.oss.model.PutObjectRequest;
+import com.aliyun.oss.model.StorageClass;
+import com.gyh.common.tools.StringUtils;
+
+import java.io.File;
+
+/**
+ * @author gyh
+ * @Date 2020/11/9 8:47
+ */
+public class OssClient {
+
+    public static void main(String[] args) {
+        // Endpoint以杭州为例，其它Region请按实际情况填写。
+        String endpoint = "http://oss-cn-beijing.aliyuncs.com";
+        // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
+        String accessKeyId = "LTAI4GFFmzpDZBmzLY1A7DUW";
+        String accessKeySecret = "pEYmVCs7nlg9n2eAWIpZ8E23nLmVmL";
+        String bucketName = "gyh7790";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        // 创建PutObjectRequest对象。
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, "imgs/head.jpg", new File("C:/Users/gyh77/Pictures/head.jpg"));
+
+        // 如果需要上传时设置存储类型与访问权限，请参考以下示例代码。
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
+        metadata.setObjectAcl(CannedAccessControlList.PublicRead);
+        metadata.setContentType("image/jpg");
+        putObjectRequest.setMetadata(metadata);
+
+        // 上传文件。
+        ossClient.putObject(putObjectRequest);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+
+        String st = "0,330600,330000,";
+        System.out.println(StringUtils.substringAfter(st,","));
+    }
+}
