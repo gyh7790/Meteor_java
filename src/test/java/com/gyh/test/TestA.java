8060eaa1c1d44f97e9b796e389f6de941671c60e
gyh7790 <gyh7790@qq.com>
2020-12-06 23:48:06 +0800

    添加 测试文件

---
+++ src/test/java/com/gyh/test/TestA.java
@@ -0,0 +1,17 @@
+package com.gyh.test;
+
+import com.gyh.common.tools.StringUtils;
+
+/**
+ * @author gyh
+ * @Date 2020/10/27 21:13
+ */
+public class TestA {
+
+    public static void main(String[] args) {
+
+        String str = "129";
+        Integer a = 129;
+        System.out.println(str.equals(a));
+    }
+}
