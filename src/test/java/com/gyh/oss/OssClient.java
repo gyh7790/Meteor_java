5e4883884ac6fd18d895ff19a5304f89f6c4a167
gyh7790 <gyh7790@qq.com>
2021-01-30 15:16:03 +0800

    修改 Oss

--- src/test/java/com/gyh/oss/OssClient.java
+++ src/test/java/com/gyh/oss/OssClient.java
@@ -21,8 +21,8 @@ public class OssClient {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = "http://oss-cn-beijing.aliyuncs.com";
         // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-        String accessKeyId = "LTAI4GFFmzpDZBmzLY1A7DUW";
-        String accessKeySecret = "pEYmVCs7nlg9n2eAWIpZ8E23nLmVmL";
+        String accessKeyId = "accessKeyId";
+        String accessKeySecret = "accessKeySecret";
         String bucketName = "gyh7790";
 
         // 创建OSSClient实例。
